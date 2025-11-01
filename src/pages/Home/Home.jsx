bb331b46f49069603876907e092964d3e881bb98
unknown <prachismitapanda10@gmail.com>
2025-11-01 20:15:37 +0530

    content added

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Home.css'
+import Header from '../../components/navbar/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
